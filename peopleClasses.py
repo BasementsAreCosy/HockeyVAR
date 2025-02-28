8ceb53a6fd17b65dd178cbfd06ce0e729a8c0ba4
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2025-02-28 09:42:39 +0000

    Created People inheritance structure

---
+++ peopleClasses.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self):
+        pass
+
+    def recordChallenge(self):
+        pass
+
+    def getSubjectData(self):
+        pass
+
+class Umpire(Person):
+    def __init__(self):
+        super.__init__()
+
+    def recordChallenge(self):
+        pass
+
+    def getSubjectData(self):
+        pass
+
+class Player(Person):
+    def __init__(self):
+        super.__init__()
