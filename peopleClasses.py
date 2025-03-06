ae6b4e41bf6d93fdca19bcb6de277f1ab57c4a6a
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2025-03-06 11:05:11 +0000

    Update peopleClasses.py

--- peopleClasses.py
+++ peopleClasses.py
@@ -1,6 +1,7 @@
 class Person:
-    def __init__(self):
-        pass
+    def __init__(self, home, away):
+        self.home = home
+        self.away = away
 
     def recordChallenge(self):
         pass
@@ -9,8 +10,8 @@ class Person:
         pass
 
 class Umpire(Person):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, home, away, isHome=None):
+        super().__init__(home, away)
 
     def recordChallenge(self):
         pass
@@ -19,5 +20,16 @@ class Umpire(Person):
         pass
 
 class Player(Person):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, home, away, isHome):
+        super().__init__(home, away)
+        self.isHome = isHome
+
+    def getTeam(self):
+        if self.isHome:
+            return self.home
+        return self.away
+
+    def getOpposition(self):
+        if self.isHome:
+            return self.away
+        return self.home
